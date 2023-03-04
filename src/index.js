407cc5ceaa170d07f65462b74b39983bf1beec16
erenaygunn <eaygun178@gmail.com>
2023-03-04 15:20:40 +0300

    initialize app

--- src/index.js
+++ src/index.js
@@ -3,14 +3,4 @@ import ReactDOM from "react-dom";
 
 import App from "./App"
 
-const App = () => {
-    return (
-        <div className='App'>
-            <button>-</button>
-            <h1>0</h1>
-            <button>+</button>
-        </div>
-    );
-}
-
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"))
